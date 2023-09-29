ef028b3b0a88496e331655276cb05fa605621ffb
Asrul Azwan <asrulazwan90@gmail.com>
2023-09-29 23:00:36 +0800

    feat: Change Skillsets property from ICollection<Skill> to string?

--- Models/User.cs
+++ Models/User.cs
@@ -6,6 +6,6 @@ public class User
     public required string Username { get; set; }
     public required string Email { get; set; }
     public required string PhoneNumber { get; set; }
-    public required ICollection<Skill> Skillsets { get; set; }
+    public string? Skillsets { get; set; }
     public string? Hobby { get; set; }
 }
